4e9c51726fc346d9f916f8ba42be8c54c79e9095
gabkf <135331040+gabkf@users.noreply.github.com>
2023-09-29 10:46:15 -0300

    Add files via upload

---
+++ aluramidi-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,12 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('tecla_pom').onclik = tocaSomPom;
+
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('tecla_clap').onclik = tocaSomClap;
